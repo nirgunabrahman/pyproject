89d3234fdad1e2f1372e30a9ba0dce4169e2ddae
root <root@ip-10-0-0-104.ap-south-1.compute.internal>
2024-09-13 12:22:22 +0000

    modified

--- sourcecode.py
+++ sourcecode.py
@@ -52,4 +52,19 @@ print("4.Divide")
           if next_calculation == "no":
             break
       else:
-          print("Invalid Input")
+
+  print("Invalid Input")
+
+  # Python Program to find the factors of a number
+
+# This function computes the factor of the argument passed
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = 320
+
+print_factors(num)
+
